db88f42172f609ff89295d8b18f8865ec3c99024
Petrovich-A <A.Piatrovich@gmail.com>
2023-03-22 13:41:29 +0300

    added Computer

--- Computer1/src/main/java/by/petrovich/computer/Main.java
+++ Computer1/src/main/java/by/petrovich/computer/Main.java
@@ -1,7 +1,9 @@
 package by.petrovich.computer;
 
+import by.petrovich.computer.model.Computer;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         User user1 = new User("John", "Smith", 55);
         User user = new User();
         user.setName("John");
@@ -12,6 +14,11 @@ public class Main {
         user.getSurName();
         user.increaseAge(5);
         System.out.println(user);
+
+        Computer computer = new Computer("Intell4.8", "Hynix3200", "Seagate 7200 500gb");
+        System.out.println(computer);
+        computer.on();
+        System.out.println(computer);
 
     }
 
